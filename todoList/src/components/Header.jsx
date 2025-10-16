9055064629c4b9bc00ea43a3c93ca036a0d81677
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-10-15 20:38:20 -0500

    React

---
+++ todoList/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header (){
+    return(
+        <h1 style={{textDecoration: "underline" }}><strong>My To-Do List</strong></h1>
+    )
+}
---
+++ todoList/src/components/ToDoList.jsx
@@ -0,0 +1,15 @@
+export default function ToDoList(){
+    
+    return (
+        <div>
+            <div>
+            <h2>List:</h2>
+            </div>
+            <ul>
+                <li>Learn React</li>
+                <li>Build a Project</li>
+                <li>Read documentation</li>
+            </ul>
+        </div>
+    )
+}
